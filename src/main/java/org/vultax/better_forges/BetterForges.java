220b4cd57af9dd5ea9488e92403c91eadee60033
Vultax <bernardozameckiandrade@gmail.com>
2024-08-01 09:04:38 -0300

    First commit

---
+++ src/main/java/org/vultax/better_forges/BetterForges.java
@@ -0,0 +1,11 @@
+package org.vultax.better_forges;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class BetterForges implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
